78a3b7cd8d02bd92236d52d5e714bb1ec4220b62
Bryan Ehlers <bryan-ehlers@uiowa.edu>
2016-09-11 18:24:32 -0500

    finished

--- assignment-1/problem-3/problem3.c
+++ assignment-1/problem-3/problem3.c
@@ -13,53 +13,31 @@
 #define MAX_NUM_LENGTH 10000
 
 void multiply_and_print(char* num1, char* num2) {
-	int n1[MAX_NUM_LENGTH], n2[MAX_NUM_LENGTH], temp[MAX_NUM_LENGTH], product[MAX_NUM_LENGTH];
-	int i, j, t, l1, l2, carry;
+	int l1 = strlen(num1);
+	int l2 = strlen(num2);
 
-	for (i = 0; num1[i] != '\0'; i++) {
-		n1[i] = num1[i] - '0';
-	}	
-	l1 = i;
+	int * product = malloc ((l1+l2)*sizeof(int));
 
-	for (i = 0; num2[i] != '\0'; i++) {
-		n2[i] = num2[i] - '0';
-	}
-	l2 = i;
+	// zero out array just to be safe
+	memset(product, 0, sizeof(int)*(l1+l2));
 
-	int max = (l1 > l2) ? l1 : l2;
-	int min = (l1 > l2) ? l2 : l1;
-	int offset = max - min;
-
-	if (l1 > l2) {
-		for (i = 0; i < min; i++) {
-			temp[i+offset] = n2[i];
-			if (i < offset) {
-				temp[i] = 0;
-			}
+	// naive multiplication algorithm
+	for (int i = l1-1; i >= 0; i--) {
+		for (int j = l2-1; j >= 0; j--) {
+			product[i+j+1] += ((num2[j] - '0')*(num1[i] - '0'));
 		}
-		memcpy(n2, temp, sizeof(temp));
-	} else if (l2 > l1) {
-		for (i = 0; i < min; i++) {
-			temp[i+offset] = n1[i];
-			if (i < offset) {
-				temp[i];
-			}
-		}
-		memcpy(n1, temp, sizeof(temp));
 	}
-
-	for (i = l2-1; i >= 0; i--) {
-		carry = 0;
-		for (j = l1-1; j >= 0; j--) {
-			t = (n1[j] * n2[i]) + product[i+j] + carry;
-			carry = t/10;
-			product[i+j] = t % 10;
+	for (int i = (l1+l2-1); i >= 0; i--) {
+		if (product[i] >= 10) {
+			product[i-1] += (product[i]/10);
+			product[i] %= 10;	
 		}
 	}
 
-	for (i = 0; i < 2*max; i++) {
+	for (int i = 0; i < (l1+l2); i++) {
 		printf("%d", product[i]);
 	} printf("\n");
+	free(product);
 }
 
 int main (int argc , char * argv []) {
